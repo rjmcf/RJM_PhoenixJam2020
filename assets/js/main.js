5aaad3535c482723463bdb210748bdaafabd601a
rjmcf <rjmcf@live.co.uk>
2020-03-31 00:09:19 +0100

    Ensure I can load images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,17 @@
 let canvas;
 let ctx;
 
+function loadImages()
+{
+    const bedroomImg = new Image();
+    bedroomImg.src = '../assets/images/roomPlaceHolder.jpg';
+
+    background = bedroomImg;
+}
+
 function drawBackground()
 {
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(0,0, canvas.width/2,canvas.height/2);
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(canvas.width/2,0, canvas.width/2,canvas.height/2);
-    ctx.fillStyle = "#00FF00";
-    ctx.fillRect(0,canvas.height/2, canvas.width/2,canvas.height/2);
-    ctx.fillStyle = "#0000FF";
-    ctx.fillRect(canvas.width/2,canvas.height/2, canvas.width/2,canvas.height/2);
+	ctx.drawImage(background, 0,0, canvas.width,canvas.height);
 }
 
 function draw()
@@ -25,5 +26,6 @@ window.onload = function()
 {
     canvas = document.getElementById("gameCanvas");
     ctx = canvas.getContext("2d");
+    loadImages();
     draw();
 };
